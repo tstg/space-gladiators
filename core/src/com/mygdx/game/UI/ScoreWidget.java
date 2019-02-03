e4b24500b631fefd2e2c0a1557f8721822edf8ed
unknown <29978690+tstg@users.noreply.github.com>
2019-02-03 19:15:02 +0800

    UI tweening

--- core/src/com/mygdx/game/UI/ScoreWidget.java
+++ core/src/com/mygdx/game/UI/ScoreWidget.java
@@ -2,6 +2,9 @@ package com.mygdx.game.UI;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.mygdx.game.Assets;
 import com.mygdx.game.components.PlayerComponent;
@@ -11,32 +14,50 @@ import com.mygdx.game.components.PlayerComponent;
  */
 
 public class ScoreWidget extends Actor {
-    Label label;
+    private Label label;
+    private Container container;
+    private int score;
 
     public ScoreWidget() {
         label = new Label("", Assets.skin);
+        score = 0;
+        // in order to use a `Label` class with `Actions`, we will first need to add it to a
+        // `Container` class (even if the `Label` class lets you add `Actions`, they won't work on
+        // this version of LibGDX, at least.
+        container = new Container(label);
+        container.setTransform(true);
+        container.fill();
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        label.draw(batch, parentAlpha);
+        container.draw(batch, parentAlpha);
+//        label.draw(batch, parentAlpha);
     }
 
     @Override
     public void act(float delta) {
-        label.act(delta);
+        container.act(delta);
+        if (PlayerComponent.score > score) {
+            container.addAction(new SequenceAction(
+                    Actions.scaleBy(0.5f, 0.3f), Actions.scaleBy(-0.5f, -0.25f, 0.3f)));
+        }
+        score = PlayerComponent.score;
+//        label.act(delta);
         label.setText("Score : " + PlayerComponent.score);
     }
 
     @Override
     public void setPosition(float x, float y) {
         super.setPosition(x, y);
-        label.setPosition(x, y);
+        container.setPosition(x, y);
+//        label.setPosition(x, y);
     }
 
     @Override
     public void setSize(float width, float height) {
         super.setSize(width, height);
-        label.setSize(width, height);
+        container.setSize(width, height);
+//        label.setSize(width, height);
     }
 }
